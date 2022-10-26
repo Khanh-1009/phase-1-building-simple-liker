d830eef222cf3b52928786a73578cb6734fa8903
Khanh Tran <katetrannbkhanh@gmail.com>
2022-10-26 13:03:25 -0700

    commit tree d107c38c87df255489ee73c52d26160b46129beb

--- main.js
+++ main.js
@@ -3,6 +3,50 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//Recognize Events - click action, when someone clicks on the heart => color
+//change from &#x2661 to red, make sure DOM loaded
+const userAction = document.getElementsByClassName('like-glyph')
+
+
+
+function clickAction(){
+  for (const click of userAction){
+      click.addEventListener('click', userLike)
+    }
+  }
+
+clickAction();
+//Manipulate the DOM - update the screen shows that the heart is clicked
+// reject - 
+/*Display the error modal by removing the .hidden class
+Display the server error message in the modal
+Use setTimeout to hide the modal after 3 seconds (add the .hidden class)*/
+//Communicate with the Server - 
+
+
+function userLike(e){
+  mimicServerCall()
+  .then(() => {
+  let heart = e.target
+
+  if (heart.textContent === EMPTY_HEART){
+    heart.textContent = FULL_HEART
+    heart.classList.add('activated-heart')
+  }
+  else if (heart.textContent === FULL_HEART){
+    heart.textContent = EMPTY_HEART
+    heart.classList.remove('activated-heart')
+  }
+  })
+  .catch(function(error){
+    let warn = document.getElementById('modal')
+    let failure = document.getElementById('modal-message')
+    failure.innerHTML = error
+    warn.classList.remove('hidden')
+    setTimeout(() => warn.classList.add('hidden'), 3000)
+  })
+
+}
 
 
 
